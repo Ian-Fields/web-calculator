8ddbe9511e0d9da3b911b5c27b703b17e79fcd7a
Ian Fields <ianfields@ians-air.worcester.local>
2020-02-25 18:14:15 -0500

    part-one-answer including css and attempt at switch case

---
+++ part-one-answer.js
@@ -0,0 +1,17 @@
+function showresult(val){
+    var n1=parseFloat(document.getElementById('num1').value);
+    var n2=parseFloat(document.getElementById('num2').value);
+    var r=n1+n2;
+    /*var c=val;
+    
+    switch(c)
+        {
+        case '1':
+            r=n1+n2;
+            break;
+        default:
+            break;     
+        }*/
+    document.getElementById('result').value=c;
+
+    }
